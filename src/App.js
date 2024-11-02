8a9373095a3d145033990bce8aff74a67030bbbe
ra11 <rajeswarivinayagam05@gmail.com>
2024-11-02 19:08:14 +0400

    Updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const[city,setcity]=useState("")
+  const[weather,setweather]=useState("")
+  const[temp,settemp]=useState("")
+  const[desc,setdesc]=useState("")
+
+  function handleCity(evt){
+    setcity(evt.target.value)
+  }
+
+  function getWeather(){
+    var weatherData= axios(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=dcf4d9a6e37b53419fa9bd0577b185a9`);
+
+    weatherData.then(function(success){
+      console.log(success.data)
+      setweather(success.data.weather[0].main)
+      setdesc(success.data.weather[0].description)
+      settemp(success.data.main.temp)
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+   <div className="bg-black p-20">
+    <div className="bg-blue-300 p-10 rounded-md">
+    <h1>Weather Report</h1>
+    <p>I can give you weather report about your city!</p>
+    <input onChange={handleCity} type="text" className="mt-2 border border-black rounded-md"></input><br></br>
+    <button onClick={getWeather} className="bg-black text-white p-2 rounded-md mt-2">Get Report</button>
+    <div className="mt-2">
+      <h1><b>Weather:</b>{weather}</h1>
+      <p><b>Temperature:</b>{temp}</p>
+      <p><b>Description:</b>{desc}</p>
     </div>
+    </div>
+
+   </div>
   );
 }
 
